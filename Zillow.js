2e49053068cae8501d626715b940d6358b4e084f
rks170530 <47929979+rks170530@users.noreply.github.com>
2019-02-23 21:39:41 -0600

    Add ZillowAPI

--- Zillow.js
+++ Zillow.js
@@ -2,6 +2,7 @@ const Zillow = require('node-zillow'); //import node-zillow from Github
 const xml = require('xml-parse');
 const parser = require('json-parser');
 var zillow = new Zillow('X1-ZWz1838f9ga6tn_17jma');
+var fs =  require('fs');
 http://www.zillow.com/webservice/GetRegionChildren.htm?zws-id=X1-ZWz1838f9ga6tn_17jma&state=wa&city=seattle&childtype=neighborhood
 var parameters = {
 	regionId: '',
@@ -13,38 +14,49 @@ var parameters = {
 
 zillow.get('GetRegionChildren', parameters)
 	.then(function(results) {
-	
-		for(var i = 0; i<102; i++){
+		
+		var obj = {
+			table: []
+		};	
+
+
+
+		for(var i = 0; i<results.response.list.region.length; i++){
 			
+			var id = i + 1;
+
 			var zID = parseInt(results.response.list.region[i].id); // + "");
-			console.log(zID);
 			
 			var zName = (results.response.list.region[i].name + "");
-			console.log(zName);
 
 			var zURL = (results.response.list.region[i].url + "");
-			console.log(zURL);
 			
 			var zLat = parseFloat(results.response.list.region[i].latitude);
-			console.log(zLat);
 			
 			var zLong = parseFloat(results.response.list.region[i].longitude);
-			console.log(zLong);
 			
 			try {
 				
 				var zIndexPrice = (results.response.list.region[i].zindex[0]._ + "");
-				console.log(zIndexPrice);
 			}
 
 			catch(err) {
 
 				var zIndexPrice = -1;
-				console.log(zIndexPrice);
 
 			}
+
+			obj.table.push({"id": id, "zID": zID, "zName": zName, "zURL": zURL, "zLat": zLat, "zLong": zLong});
 			
 		}
+		console.log(obj.table);
+		
+		fs.writeFile ("zillowParse.json", JSON.stringify(obj), function(err) {
+   	 	if (err) throw err;
+    		console.log('complete');
+    }
+);
+
 		
 		
 	})
